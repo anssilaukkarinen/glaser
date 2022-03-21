dd6b7016bb9e471335f68ba6e9d190a859bc303c
Anssi Laukkarinen <anssi.laukkarinen@tuni.fi>
2022-03-21 21:03:26 +0200

    Update RHmax calculation

--- glaser.py
+++ glaser.py
@@ -11,11 +11,11 @@ import matplotlib.pyplot as plt
 
 class Glaser():
     
-    def __init__(self, layers_list_input, BC_dict_input, RHmin_input):
+    def __init__(self, layers_list_input, BC_dict_input):
         
         ## constants
         # Surface resistances
-        self.Rse = 0.04
+        self.Rse = 0.13
         self.Rsi = 0.13
         self.sdse = 0.0
         self.sdsi = 0.0
@@ -23,59 +23,78 @@ class Glaser():
         self.dsurf = 0.05
         self.R_layer_max = 0.05
         
+        self.delta_v_air = 0.000025
+        
         
         ## initialisations
         # check that layers have R <= 0.2 m2K/W
         self.layers_list_input = layers_list_input
         self.prepare_layers()
         
-        # Calculate indoor and outdoor vapor concentration
+        
+        # Calculate indoor and outdoor vapor concentrations for each time step
         self.Te = BC_dict_input['Te']
         self.RHe = BC_dict_input['RHe']
         self.ve = (self.RHe/100.0) * self.calc_vsat(self.Te)
         
-        self.Ti = BC_dict_input['Ti']
-        self.vi = self.ve + self.calc_dv(self.Te)
+        self.Ti = self.calc_Ti(BC_dict_input['Ti'])
+        self.vi = self.calc_vi(BC_dict_input['vi'])
         
         self.dT_tot = self.Ti - self.Te
         self.dv_tot = self.vi - self.ve
         
-        # Mould growth potential calculations
-        self.RHmin = RHmin_input
+        self.dt_hours = BC_dict_input['dt_hours']
+        
         
         
         ## calculations
-        self.calc_Tn_vsat()
         
-        self.calc_vn_phi()
+        self.main()
         
-        self.calc_M_potential()
+
         
         
         
     
     @staticmethod
     def calc_vsat(T):
+        # SFS-EN ISO 13788
         # [T] = degC
-        pvsat = 610.5 * np.exp((17.269*T)/(237.3+T))
-        vsat = pvsat/(462.0*(273.15+T))
+        # vsat is calculated over ice in subzero temperatures
+        if type(T) == np.float64:
+            T = np.array([T])
+        
+        vsat = np.zeros(T.shape)
+        for idx_Tval, Tval in enumerate(T):
+            if Tval >= 0.0:
+                # water
+                pvsat = 610.5 * np.exp((17.269*Tval)/(237.3+Tval))
+                vsat[idx_Tval] = pvsat/(462.0*(273.15+Tval))
+            else:
+                # ice
+                pvsat = 610.5 * np.exp((21.875*Tval)/(265.5+Tval))
+                vsat[idx_Tval] = pvsat/(462.0*(273.15+Tval))
+        
         return(vsat)
     
     
     @staticmethod
     def calc_dv(Te):
-        if Te < 5.0:
-            dv = 0.005
-        elif Te > 15.0:
-            dv = 0.002
-        else:
-            dv = (6.5 - 0.3*Te) / 1000.0
-    
+        dv = []
+        for Te_val in Te:
+            if Te < 5.0:
+                dv.append(0.005)
+            elif Te > 15.0:
+                dv.append(0.002)
+            else:
+                dv.append((6.5 - 0.3*Te) / 1000.0)
+        dv = np.array(dv)
         return(dv)
     
     
     @staticmethod
-    def RH_crit(T, RHmin):
+    def RH_crit(T):
+        RHmin = 80.0
         
         RHcrit = np.zeros(len(T))
         
@@ -84,19 +103,23 @@ class Glaser():
                 RHcrit[idx_Tval] = 100.0
             
             elif Tval <= 20.0:
-                RHcrit[idx_Tval] = -0.00267*Tval**3 \
-                                   + 0.160*Tval**2 \
-                                   -3.13*Tval \
-                                   +100.0
-            else:
+                dummy = -0.00267*Tval**3 \
+                        +0.160*Tval**2 \
+                        -3.13*Tval \
+                        +100.0
+                RHcrit[idx_Tval] = np.max((dummy, RHmin))
+            
+            elif Tval <= 50.0:
                 RHcrit[idx_Tval] = RHmin
+            
+            else:
+                RHcrit[idx_Tval] = 100.0
         
         return(RHcrit)
         
-    
-    
 
 
+    
 
     def prepare_layers(self):
         
@@ -104,7 +127,9 @@ class Glaser():
         self.layers_list_R = []
         self.layers_list_sd = []
         
-        for layer in self.layers_list_input:
+        self.interface_idxs = []
+        
+        for idx_layer, layer in enumerate(self.layers_list_input):
             
             if 'd' in layer.keys():
                 d = layer['d']
@@ -118,19 +143,23 @@ class Glaser():
                 lam = d / R
                 mu = layer['sd'] / d
             
-            # divide into shorter distances, if necessary
-            if R <= self.R_layer_max:
-                self.layers_list_d.append(d)
-                self.layers_list_R.append(d / lam)
-                self.layers_list_sd.append(d * mu)
-            else:
-                n = np.ceil(R/self.R_layer_max).astype(int)
-                for idx in range(n):
-                    
-                    self.layers_list_d.append(d/n)
-                    self.layers_list_R.append((d/n) / lam)
-                    self.layers_list_sd.append((d/n) * mu)
+            n = 50
             
+            # create a list of resistances
+            for idx in range(n):
+                self.layers_list_d.append(d/n)
+                self.layers_list_R.append((d/n) / lam)
+                self.layers_list_sd.append((d/n) * mu)
+            
+            # create a list of interface indexis for mould growth risk calculation
+            # the last round is for interior surface, which is included
+            self.interface_idxs.append(1 + (idx_layer+1) * n)
+            
+            # determine single interface index for evaporation calculation
+            if 'evap_layer' in layer.keys():
+                self.idx_evap_layer = 1 + len(self.layers_list_d)
+            
+        
         # add surface resistances
         self.layers_list_d.insert(0, self.dsurf)
         self.layers_list_R.insert(0, self.Rse)
@@ -152,69 +181,230 @@ class Glaser():
         self.sd_cum = np.concatenate( (np_zero_array,
                                        np.cumsum( self.layers_list_sd )) )
         
+        self.R_tot = self.R_cum[-1]
+        self.sd_tot = self.sd_cum[-1]
+
+
+    def calc_Ti(self, arg1):
+        if arg1 == 'Ti_const_21':
+            Ti = 21.0 * np.ones(self.Te.shape)
+        
+        elif arg1 == 'Ti_const_18':
+            Ti = 18.0 * np.ones(self.Te.shape)
+            
+        else:
+            print('Unknown Ti arg1')
+            Ti = np.nan
+        return(Ti)
+    
+    
+
+    def calc_vi(self, arg1):
+        if arg1 == 'RIL107_2012':
+            vi = self.ve + self.calc_dv(self.Te)
+        elif arg1 == 'RHi_const_50':
+            vi = (50.0/100.0) * self.calc_vsat( self.Ti )
+        else:
+            print('Unknown arg1!')
+            vi = np.nan
+        return(vi)
+
+
+    @staticmethod
+    def func_list_condensation_ranges(RHx):
+        # This function counts the number of condensation regions
+        # inside the structure
+        
+        local_variable_list_condensation_ranges = []
+        counter = 0
+        
+        while counter < len(RHx):
+            
+            if RHx[counter] > 100.0:
+                # condensation range starts
+                
+                idx_exterior = counter
+                idx_interior = counter
+                
+                counter = counter + 1
+                
+                while counter < len(RHx) and RHx[counter] > 100.0:
+                    idx_interior = counter
+                    
+                    counter = counter + 1
+                
+                # The condensation range has ended
+                condensation_range = {'exterior': idx_exterior,
+                                      'interior': idx_interior}
+                local_variable_list_condensation_ranges.append(condensation_range)
+                # print('cond range', condensation_range)
+                
+            counter = counter + 1
+        
+        return(local_variable_list_condensation_ranges)
+
+
+
+
+
+
+    def main(self):
+        
+        self.gcond = np.zeros(self.Te.shape)
+        self.gevap = np.zeros(self.Te.shape)
+        self.RHmax = np.zeros(self.Te.shape)
+        self.mpotmax = np.zeros(self.Te.shape)
+        
+        for idx, Te_val in enumerate(self.Te):
+            
+            Te = self.Te[idx]
+            ve = self.ve[idx]
+            Ti = self.Ti[idx]
+            vi = self.vi[idx]
+            dt = self.dt_hours[idx] * 3600.0
+            
+            dT_tot = Ti - Te
+            dv_tot = vi - ve
+            
+            # Temperature and saturation concentration
+            Tn = Te + (self.R_cum/self.R_cum[-1]) * dT_tot
+            vsatn = self.calc_vsat(Tn)
+            
+            ## Vapor concentration and relative humidity
+            # Round 1
+            vn = ve + (self.sd_cum/self.sd_cum[-1]) * dv_tot
+            phin = 100.0 * (vn / vsatn)
+            # print(idx, self.Te[idx].round(1),
+            #           self.RHe[idx].round(1),
+            #           phin.max().round(1))
+            
+            
+            
+            # Go through relative humidity values and see, if there is condensation
+            list_condensation_ranges = self.func_list_condensation_ranges(phin)
+            
+            n_cond_ranges = len(list_condensation_ranges)            
+            
+            ##
+            if n_cond_ranges == 0:
+                
+                # amount of condensation (no condensation)
+                self.gcond[idx] = 0.0
+                
+                # amount of evaporation
+                T_evap = Tn[self.idx_evap_layer]
+                v_sat_evap = self.calc_vsat(T_evap)
+
+                dv_interior = vi - v_sat_evap
+                sd_interior = self.sd_cum[-1] - self.sd_cum[self.idx_evap_layer]
+                g_in = self.delta_v_air * (dv_interior / sd_interior)
+                
+                dv_exterior = v_sat_evap - ve
+                sd_exterior = self.sd_cum[self.idx_evap_layer]
+                g_out = self.delta_v_air * (dv_exterior / sd_exterior)
+                
+                self.gevap[idx] = (g_in - g_out) * dt * 1000.0
+                # print('gevap = ', self.gevap[idx].round(1), 'g/m2')
+                
+                
+                # maximum relative humidity
+                self.RHmax[idx] = phin[self.interface_idxs].max()
+                
+                
+                # mould growth potential at material interfaces
+                T_M = Tn[self.interface_idxs]
+                phi_M = phin[self.interface_idxs]
+                phi_M_min = self.RH_crit(T_M)
+                
+                m_pot = phi_M / phi_M_min
+                # print(m_pot.round(2))
+                self.mpotmax[idx] = m_pot.max()
+                
+                
+                
+            
+            
+            elif n_cond_ranges == 1:                
+                # There is only one condensation range in the structure
+
+                # amount of condensation
+                idxs_phi_high = phin > 100.0
+                idx_exterior_border = idxs_phi_high.argmax()
+                idx_interior_border = (idxs_phi_high.shape[0] - 1) - idxs_phi_high[::-1].argmax()
+                
+                v_exterior_border = self.calc_vsat(Tn[idx_exterior_border])
+                v_interior_border = self.calc_vsat(Tn[idx_interior_border])
+                
+                dv_exterior = v_exterior_border - ve
+                sd_exterior = self.sd_cum[idx_exterior_border]
+                g_out = self.delta_v_air * (dv_exterior / sd_exterior)
+                
+                dv_interior = vi - v_interior_border
+                sd_interior = self.sd_cum[-1] - self.sd_cum[idx_interior_border]
+                g_in = self.delta_v_air * (dv_interior / sd_interior)
+                
+                self.gcond[idx] = (g_in - g_out) * dt * 1000.0
+                
+                # amount of evaporation
+                self.gevap[idx] = 0.0
+                #print('g_cond_net =', self.gcond[idx].round(1), 'g/m2')
+                
+                
+                # update vapor concentration
+                # condensation region
+                vn_limited = vn.copy()
+                vn_limited[idx_exterior_border:idx_interior_border] \
+                    = vn[idx_exterior_border:idx_interior_border]
+                
+                # exterior side
+                sd_vals = self.sd_cum[:idx_exterior_border]
+                vn_limited[:idx_exterior_border] \
+                    = ve + (sd_vals/sd_vals[-1]) * dv_exterior
+                
+                # interior side
+                sd_vals = self.sd_cum[idx_interior_border:] - self.sd_cum[idx_interior_border]
+                vn_limited[idx_interior_border:] \
+                    = v_interior_border + (sd_vals/sd_vals[-1]) * dv_interior
+                
+                
+                # relative humidity
+                phin_limited = vn_limited / vsatn
+                self.RHmax[idx] = phin_limited[self.interface_idxs].max()
+                
+                                
+                # mould growth potential at material interfaces                
+                T_M = Tn[self.interface_idxs]
+                phi_M = phin_limited[self.interface_idxs]
+                phi_M_min = self.RH_crit(T_M)
+                
+                m_pot = phi_M / phi_M_min
+                # print(m_pot.round(2))
+                self.mpotmax[idx] = m_pot.max()
+                
+            
+            else:
+                print('number of condensation ranges per structure:', n_cond_ranges)
+
+                
+                
+        self.mcond = self.gcond.sum()
+        self.mevap = self.gevap.sum()
+        self.RHmaxq100 = np.quantile(self.RHmax, 1.0)
+        self.RHmaxq90 = np.quantile(self.RHmax, 0.9)
+        self.mpotmaxq100 = np.quantile(self.mpotmax, 1.0)
+        self.mpotmaxq90 = np.quantile(self.mpotmax, 0.9)
+    
+
+
+        
+    def print_info(self):
+        
         print('Rtot =', self.R_cum[-1].round(2),
               'U =', np.round(1/self.R_cum[-1], 3),
               'sdtot =', self.sd_cum[-1].round(2))
         
-    
-    def calc_Tn_vsat(self):
         
-        self.Tn = self.Te \
-            + (self.R_cum /self.R_cum[-1]) * self.dT_tot
-        
-        self.vsatn = self.calc_vsat(self.Tn)
-        
-    
-    def calc_vn_phi(self):
-        
-        self.vn = self.ve \
-            + (self.sd_cum / self.sd_cum[-1]) * self.dv_tot
-        
-        self.phin = 100.0 * (self.vn / self.vsatn)
-    
-    
 
-    def calc_M_potential(self):
-        
-        self.Mpotn = self.phin / self.RH_crit(self.Tn, self.RHmin)
-        
-        
-        
-        
-    
-    def plot_basics(self):
-        
-        fig, ax = plt.subplots(figsize=(6,4))
-        ax.plot(self.d_cum, self.Tn, '-o')
-        ax.grid(True)
-        ax.set_xlabel('Etäisyys ulkopinnasta, m')
-        ax.set_ylabel('Tn, C')
-        
-        fig, ax = plt.subplots(figsize=(6,4))
-        ax.plot(self.d_cum, self.vsatn, '-o')
-        ax.plot(self.d_cum, self.vn, '-s')
-        ax.grid(True)
-        ax.set_xlabel('Etäisyys ulkopinnasta, m')
-        ax.set_ylabel('v, kg/m3')
-        ax.legend(['vsat', 'v'])
-    
-        fig, ax = plt.subplots(figsize=(6,4))
-        ax.plot(self.d_cum, self.phin, '-o')
-        ax.grid(True)
-        ax.set_xlabel('Etäisyys ulkopinnasta, m')
-        ax.set_ylabel('phin, %')
-    
-        fig, ax = plt.subplots(figsize=(6,4))
-        ax.plot(self.d_cum, self.phin/self.RH_crit(self.Tn, self.RHmin), '-o')
-        ax.grid(True)
-        ax.set_xlabel('Etäisyys ulkopinnasta, m')
-        ax.set_ylabel('Mpot, -')
-    
-    
-    
-    
-    
-    
     
     
     
--- main.py
+++ main.py
@@ -8,42 +8,226 @@ import os
 import pickle
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+
+import statsmodels.api as sm
 
 import glaser
 
-## puurankaseinä
-# layers_list = [{'d':0.009,
-#                 'lam': 0.21,
-#                 'mu': 4},
-#                 {'d':0.25,
+
+fname = os.path.join(r'C:\Local\laukkara\Data\RASMI-datat\2 pickle',
+                     'RASMI.pickle')
+with open(fname, 'rb') as f:
+    data = pickle.load(f)
+
+
+output_folder = os.path.join(r'C:\Local\laukkara\Data\github\glaser',
+                             'output')
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+
+
+layers_list_name = 'timber_frame_wall_new'
+layers_list = [{'d':0.009,
+                'lam': 0.21,
+                'mu': 4,
+                'evap_layer': True},
+                {'d':0.198,
+                'lam': 0.037,
+                'mu': 1},
+                {'R': 0.003,
+                'sd': 100.0},
+                {'d':0.048,
+                'lam': 0.037,
+                'mu': 1},
+                {'d':0.0125,
+                'lam': 0.21,
+                'mu': 10}]
+
+
+# layers_list_name = 'timber_frame_wall_old'
+# layers_list = [{'d':0.025,
+#                 'lam': 0.13,
+#                 'mu': 50,
+#                 'evap_layer': True},
+#                 {'R': 0.003,
+#                 'sd': 0.5},
+#                 {'d':0.125,
+#                 'lam': 0.06,
+#                 'mu': 1.5},
+#                 {'R': 0.003,
+#                 'sd': 0.5},
+#                 {'d':0.025,
+#                 'lam': 0.13,
+#                 'mu': 50},
+#                 {'d':0.01,
+#                 'lam': 0.1,
+#                 'mu': 50}]
+
+
+# layers_list_name = 'log_wall_interior_insulation'
+# layers_list = [{'d':0.18,
+#                 'lam': 0.13,
+#                 'mu': 40,
+#                 'evap_layer': True},
+#                 {'d':0.1,
 #                 'lam': 0.039,
 #                 'mu': 1},
-#                 {'R': 0.001,
-#                 'sd': 0.0005},
-#                 {'d':0.0125,
+#                 {'R': 0.003,
+#                 'sd': 0.5},
+#                 {'d':0.013,
 #                 'lam': 0.21,
 #                 'mu': 10}]
 
-## betonisandwich
-layers_list = [{'d':0.07,
-                'lam': 2.3,
-                'mu': 80},
-                {'d':0.25,
-                'lam': 0.039,
-                'mu': 60},
-                {'d':0.15,
-                'lam': 2.3,
-                'mu': 130}]
 
-BC = {'Te': 0.0,
-      'RHe': 90.0,
-      'Ti': 17.0,
-      'dv': 'RIL107_2012'}
 
-RHmin = 85.0
+# layers_list_name = 'concrete_sandwich_panel'
+# layers_list = [{'d':0.07,
+#                 'lam': 2.3,
+#                 'mu': 80,
+#                 'evap_layer': True},
+#                 {'d':0.25,
+#                 'lam': 0.039,
+#                 'mu': 1},
+#                 {'d':0.15,
+#                 'lam': 2.3,
+#                 'mu': 130}]
 
-obj = glaser.Glaser(layers_list,
-                    BC,
-                    RHmin)
 
-obj.plot_basics()
+
+
+# layers_list_name = 'brick_MW_brick'
+# layers_list = [{'d':0.13,
+#                 'lam': 0.6,
+#                 'mu': 10,
+#                 'evap_layer': True},
+#                 {'d':0.15,
+#                 'lam': 0.04,
+#                 'mu': 1},
+#                 {'d':0.13,
+#                 'lam': 0.6,
+#                 'mu': 16}]
+
+
+
+#########################################
+
+
+keys = ['Jok_1989-2018',
+        'Jyv_1989-2018',
+        'Sod_1989-2018',
+        'Van_1989-2018']
+
+# keys = ['Jok_1989-2018']
+
+fname = os.path.join(output_folder,
+                     'output_{}.xlsx'.format(layers_list_name))
+writer = pd.ExcelWriter(fname)
+
+res = {}
+
+for key in keys:
+    
+    res[key] = {}
+    
+    print(key)
+    Te_all = data[key].groupby(['YEAR','MON']).mean().loc[(1989,7):(2018,6), 'Te'].values
+    RHe_all = data[key].groupby(['YEAR','MON']).mean().loc[(1989,7):(2018,6), 'RHe_ice'].values
+    dt_hours = data[key].groupby(['YEAR','MON']).count().loc[:,'Te'].values
+    
+    # Te_all = data[key].groupby(['YEAR','MON']).mean().loc[:, 'Te'].values
+    # RHe_all = data[key].groupby(['YEAR','MON']).mean().loc[:, 'RHe_water'].values
+    
+    n_years = int(Te_all.shape[0] / 12)
+    
+    obj_list = []
+    mcond_list = []
+    mevap_list = []
+    RHmaxq100_list = []
+    RHmaxq90_list = []
+    mpotmaxq100_list = []
+    mpotmaxq90_list = []
+    
+    for idx_year in range(n_years):
+        
+        Te = Te_all[ (idx_year*12) : ((idx_year+1)*12) ]
+        RHe = RHe_all[ (idx_year*12) : ((idx_year+1)*12) ]
+
+        BC = {'Te': Te,
+              'RHe': RHe,
+              'Ti': 'Ti_const_21',
+              'vi': 'RHi_const_50',
+              'dt_hours': dt_hours}
+        
+        obj = glaser.Glaser(layers_list,
+                            BC)
+        obj_list.append(obj)
+        mcond_list.append(obj.mcond)
+        mevap_list.append(obj.mevap)
+        RHmaxq100_list.append(obj.RHmaxq100)
+        RHmaxq90_list.append(obj.RHmaxq90)
+        mpotmaxq100_list.append(obj.mpotmaxq100)
+        mpotmaxq90_list.append(obj.mpotmaxq90)
+    
+    res[key]['obj_list'] = obj_list
+    
+    # version 1: This has all variables
+    df = pd.DataFrame(data={'year': np.arange(1989, 1989+n_years,),
+                        'mcond': mcond_list,
+                        'mevap': mevap_list,
+                        'RHmaxq100': RHmaxq100_list,
+                        'RHmaxq90': RHmaxq90_list,
+                        'mpotmaxq100': mpotmaxq100_list,
+                        'mpotmaxq90': mpotmaxq90_list})
+    
+    # version 2: This has a subset of variables
+    # df = pd.DataFrame(data={'year': np.arange(1989, 1989+n_years,),
+    #                 'mcond': mcond_list,
+    #                 'RHmaxq100': RHmaxq100_list,
+    #                 'RHmaxq90': RHmaxq90_list})
+    
+    
+    
+    df.index.name = 'index'
+    
+    res[key]['df'] = df
+    
+    
+    # When there is condensation, relative humidity is 100 % RH and the results saturate
+    # Also the mould growth potential saturates towards 100/80 = 1.2
+    # If there is condensation, ranked by condensation
+    # If there is no condensation, ranked by RHmazq90
+    # n_cond = (df['mcond'] > 0).sum()
+    # if n_cond > 10:
+    #     by_key = 'mcond'
+    # else:
+    #     by_key = 'RHmaxq90'
+    
+    df.sort_values(by=['mcond', 'RHmaxq100', 'RHmaxq90'],
+                   ascending=False).round(1).to_excel(writer,
+                                                        index=False,
+                                                        sheet_name=key)
+
+writer.save()
+
+
+
+#############################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
